efff1ad8eadf18c2918e91a849c325636d71443e
Stephen White <SteveWhite.dev@gmail.com>
2020-07-25 20:53:59 -0700

    abstracting server logic

--- app/server/server.js
+++ app/server/server.js
@@ -1,6 +1,6 @@
 
 const express = require("express");
-const index = require("../routes/index");
+const index = require("../routes/index.js");
 const http = require("http");
 const socketIo = require("socket.io");
 const path       = require('path');
@@ -13,38 +13,64 @@ const server = http.createServer(app);
 const io = socketIo(server);
 let clients = 0;
 
-
-io.on('connection', socket => {
-    console.log("New client connected");
+const addNewClientConnectListener = socket => {
     clients++;
+    console.log("New client connected");
     console.log('Number of connected clients: ' + clients);
-
     socket.broadcast.emit('newclientconnect',
         { numClients: clients }
     );
+}
 
+const handleClientDisconnect = socket => {
+    console.log("Client disconnected");
+    clients--;
+    console.log('Number of connected clients: ' + clients);
+    socket.broadcast.emit('clientDisconnect', { numClients: clients});
+}
+
+const addStreamListener = socket => {
     socket.on('stream', data => {
         socket.broadcast.emit('stream',data);
     });
 
+    io.of('/stream').clients((error, clients) => {
+        if (error) {
+            throw error;
+        }
+          console.log(clients);
+      });
+}
+
+const addNumClientsListener = socket => {
     socket.on('getNumClients', () => {
         socket.broadcast.emit('newclientconnect', { numClients: clients });
-    })
-   
-
-    socket.on("disconnect", () => {
-      console.log("Client disconnected");
-      clients--;
-      console.log('Number of connected clients: ' + clients);
     });
+}
+
+const addClientDisconnectListener = socket => {
+    socket.on("disconnect", () => {
+        handleClientDisconnect(socket);
+    });
+}
+
+const addSocketListeners = socket => {
+
+    addNewClientConnectListener(socket);
+
+    addStreamListener(socket);
+
+    addNumClientsListener(socket);
+    
+    addClientDisconnectListener(socket);
+    
+}
+
+io.on('connection', socket => {
+    addSocketListeners(socket);
 });
 
-io.of('/stream').clients((error, clients) => {
-  if (error) {
-      throw error;
-  }
-    console.log(clients);
-});
+
 
 server.listen(port, () => console.log(`\x1b[40m`,`\x1b[32m`,
 `
